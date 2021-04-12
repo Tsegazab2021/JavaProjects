9b50811c7ae119aec224f517942785e4475d904c
sgebr <sgebr@192.168.0.18>
2021-04-11 20:16:08 -0400

    Commit

--- JaveExercise1/src/tutorial/JavaFirstProg.java
+++ JaveExercise1/src/tutorial/JavaFirstProg.java
@@ -7,7 +7,7 @@ public class JavaFirstProg {
 
 				//Write a Java program to print 'Hello' on screen and then print your name on a separate line
 			
-			public static void main(String[] args) {
+			
 				System.out.println("Hello World");
 				
 				System.out.println("Sami Berihu");
